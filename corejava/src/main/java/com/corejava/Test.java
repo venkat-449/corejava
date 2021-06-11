098f9b97ba97ea4922e5168026f0bb6e042e2120
DELL <DELL@DESKTOP-EQQNBQS>
2021-06-11 08:00:14 +0530

    add project setup

---
+++ corejava/src/main/java/com/corejava/Test.java
@@ -0,0 +1,11 @@
+package com.corejava;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("core java projects");
+
+		System.err.println("java projects explanation");
+	}
+}
